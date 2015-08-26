93548135870e3dc9bafb02dbfab78f8781b30424
Will Field <willjfield@gmail.com>
2015-08-25 22:57:58 -0400

    a few more instructions

--- itp-irc/script.js
+++ itp-irc/script.js
@@ -1,11 +1,11 @@
 $('document').ready(function(){
 	var isOpen = false;
 	var browserHTML = "<p class='btn'>The quickest and easiest way to connect is in the browser.<br><br><b>Step 1:</b> <a href='https://kiwiirc.com/client' target='_blank'>Visit kiwiirc</a><br><br><b>Step 2:</b> Enter any nickname you'd like.<br><br><b>Step 3:</b> Enter #ITP in the channel.</p>";
-	var osxHTML = "<p class='btn'>This is a full paragraph about how you install an irc client for mac osx. Step 1: Step2: Step 3: Step4:Anuthah what a cahd paypuh bowee gummah If you can't stand the wintah you don't deserve the summah numb dingy gettin' ugly over t'.</p>";
-	var windowsHTML = "<p class='btn'>This is a full paragraph about how you install an irc client for windows.Step 1: Step2: Step 3: Step4:Over t' muckle riyht on'ta her You is sum wicked suhmart kid.</p>";
+	var osxHTML = "<p class='btn'>Colloquy is the recommended OSX IRC client.<br><br><b>Step 1: </b><a href='http://colloquy.info/downloads/colloquy-latest.zip'>Download Colloquy</a><br><br><b>Step 2: </b>Open Colloquy. Go to <br> File->New Connection... (&#8984; + N) <br><br><b>Step 3: </b> Match these settings:<br> (pick any username and nickname you'd like)<br><img src='images/colConnection.png'></p>";
+	var windowsHTML = "<p class='btn'>Instructions pending...</p>";
 	var cliHTML = "<p class='btn'>Clone the BitchX repo and compile!<br><br><b>Step 1: </b>git clone https://github.com/BitchX/BitchX1.2 BitchX<br><br><b>Step 2: </b>cd BitchX<br><br><b>Step 3: </b>./configure --with-ssl --with-plugins --enable-ipv6<br><br><b>Step 4: </b>make<br><br><b>Step 5: </b>sudo make install<br><br><a href='http://www.tc.umn.edu/~hick0088/files/BitchX.txt' target='_blank'>BITCHX USER GUIDE</a> for commands and help.</p>";
-	var iosHTML ="<p class='btn'>This is a full paragraph about how you install an irc client for ios.Step 1: Step2: Step 3: Step4:Over t' muckle riyht on'ta her You is sum wicked suhmart kid.</p>";
-	var androidHTML = "<p class='btn'>This is a full paragraph about how you install an irc client for android.Step 1: Step2: Step 3: Step4:Over t' muckle riyht on'ta her You is sum wicked suhmart kid.</p>";
+	var iosHTML ="<p class='btn'>Instructions pending...</p>";
+	var androidHTML = "<p class='btn'>AndChat is the recommended Android client for IRC. <br><br><b>Step 1: </b> <a href='https://play.google.com/store/apps/details?id=net.andchat&hl=en' target='_blank'><br><br> <img src='images/gplay.svg' width=200px></a><br><br><b>Step 2: </b> Click to create a new server connection. <br><br> <img src='images/mainAnd_h.png' width=250px> <br><br><b>Step 3: </b> Enter the following settings: <br><br>Name: Freenode<br>Address: chat.freenode.net<br>Port: 6667<br>Nick: [PICK A NICKNAME]<br>Username: [PICK A USERNAME]<br>Autojoin List: #itp</p>";
 
 
 	$(window).scroll(function(){
